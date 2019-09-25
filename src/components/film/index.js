69c7f7478df75a60dacc3d12db5536bfceac6e53
Funcu <diego.n.salinas@live.com>
2019-09-24 23:44:21 -0300

    Conected pages - Trailer Fecth

--- src/components/film/index.js
+++ src/components/film/index.js
@@ -1,28 +1,74 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import './styles.css';
+
+import ReactPlayer from 'react-player'
 
 class Films extends Component {
   constructor() {
     super();
     this.state = {
-        page: 1,
         api_key: 'f7935cd563a9169522a96c62d2fd7106',
-        films: [],
+        trailer_key: '',
+        title: '',
+        description: '',
+        fetch_status: '',
     }
   }
 
   render(){
-
     return(
-        <h1>T</h1>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-md-16">
+              <div>
+                <ReactPlayer url={"https://www.youtube.com/watch?v=" + this.state.trailer_key} allow="autoplay" playing/>
+              </div>
+            <div>
+                <h1>Film</h1>
+                <p>Lorem ipsum dolor sit a </p>
+              </div>
+          </div>
+        </div>
+      </div>
     );
   }
 
   componentDidMount(){
-
+    
+    const {match} = this.props;
+    let id = match.params.id;
+    
+    this.fetchTrailer(id);
+    this.getDetails(id);
   }
 
-  fetchFilms(){
+  getDetails(id){
+    //Get from redux
+  }
+
+  fetchTrailer(id){
+    this.setState({
+      fetch_status: 'FETCHING',
+    });
+    fetch("http://api.themoviedb.org/3/movie/"+ id +"/videos?api_key=" + this.state.api_key)
+    .then(res => res.json())
+    .then((data) => {
+      console.log('trailer_fetch', data);
+      if(!data.hasOwnProperty('results')){ //Si el resultado no tiene results -> ERROR
+        this.setState({
+          fetch_status: 'ERROR',
+        });
+      }
+      else{
+        this.setState({
+          trailer_key: data.results[0].key,
+          fetch_status: 'OK',
+        })
+      }
+    })
+    .catch(console.log)
   }
 }
 
-export default Films;
+export default withRouter(Films);
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 import './styles.css';
 
+import {withRouter} from 'react-router-dom';
+
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-        page: 54,
+        page: 1,
         api_key: 'f7935cd563a9169522a96c62d2fd7106',
         films: [],
     }
+    this.fetchFilms = this.fetchFilms.bind(this);
   }
 
   render(){
-    const films = this.state.films.map((film, i) => {
+    const { history }  = this.props;
+
+    console.log(this.state.films)
+    const display_films = this.state.films.map((film, i) => {
       return (
         <div className="col-md-4" key = {i}>
           <div className="card mt-4">
@@ -29,7 +35,8 @@ class Home extends Component {
             <div className="card-footer">
               <div className="d-flex justify-content-center">
                 <button
-                    className="btn btn-primary">
+                    className="btn btn-primary"
+                    onClick={() => history.push('/film/' + film.id)}>
                     Watch
                 </button>
               </div>
@@ -40,13 +47,18 @@ class Home extends Component {
     });
 
     return(
-      <div className="App">
-         <div className="container">
-          <div className="row mt-4">
-            <div className="col-md-16">
-              <div className="row">
-                {films}
-              </div>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-md-16">
+            <div className="row">
+              {display_films}
+            </div>
+            <div className="p-5 text-center">
+              <button
+                    className="btn btn-success"
+                    onClick={this.fetchFilms}>
+                    Load More
+              </button>
             </div>
           </div>
         </div>
@@ -62,11 +74,13 @@ class Home extends Component {
     fetch('https://api.themoviedb.org/3/discover/movie?api_key='+ this.state.api_key +'&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=' + this.state.page)
     .then(res => res.json())
     .then((data) => {
-      this.setState({ films: data.results })
-      console.log(data.results)
+      this.setState({ 
+        films: this.state.films.concat(data.results),
+        page: this.state.page+1,
+      })
     })
     .catch(console.log)
   }
 }
 
-export default Home;
+export default withRouter(Home);
